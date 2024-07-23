32ebba43f206334b97ae11a75955dc269d0a08cd
madni korejo <madnikorejo9@gmail.com>
2024-07-23 13:02:44 +0500

    Added some test code.

--- FYP-Project/api/main.py
+++ FYP-Project/api/main.py
@@ -1,1 +1,16 @@
-print("hello")
+from fastapi import FastAPI, UploadFile, File
+import uvicorn
+app = FastAPI()
+@app.get("/ping")
+async def ping():
+    return "hello I'm Alive"
+
+@app.post("/predict")
+async def predict(
+        file: UploadFile = File(...)
+):
+    pass
+
+
+if __name__ == "__main__":
+    uvicorn.run(app=app, host="localhost", port=8000)
