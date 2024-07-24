81ae04014a9d6a4e333f7312408cd01209e56721
madni korejo <madnikorejo9@gmail.com>
2024-07-24 15:21:16 +0500

    Added some code

--- FYP-Project/api/main.py
+++ FYP-Project/api/main.py
@@ -1,14 +1,35 @@
 from fastapi import FastAPI, UploadFile, File
 import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
+
+MODEL = tf.keras.models.load_model("../models/my_model.h5")
+CLASS_NAMES = ["Early Blight","Late Blight","Healthy"]
+
+
 app = FastAPI()
+
+
 @app.get("/ping")
 async def ping():
     return "hello I'm Alive"
+
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
 
 @app.post("/predict")
 async def predict(
         file: UploadFile = File(...)
 ):
+    image = read_file_as_image(await file.read())
+    image_batch = np.expand_dims(image,0)
+    prediction = MODEL.predict(image_batch)
     pass
 
 
